7cc92ce782614b45655b1d54bb87e4a78eb8e6a8
Sankalp Singha <sankalpsingha@gmail.com>
2019-07-05 16:39:21 +0530

    Added the fonts internally

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     
-    <link href="https://db.onlinewebfonts.com/c/b26b20369adfef2b3d65d266e0625fe2?family=Gabriola" rel="stylesheet" type="text/css"/>
+    <!-- <link href="https://db.onlinewebfonts.com/c/b26b20369adfef2b3d65d266e0625fe2?family=Gabriola" rel="stylesheet" type="text/css"/> -->
     <!-- <link rel="stylesheet" href="css/style.css"> -->
     <?php wp_head(); ?>
 </head>
