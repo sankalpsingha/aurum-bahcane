fcc14e80235b9c19a06e30a3ab259884631a9a9c
Sankalp Singha <sankalpsingha@gmail.com>
2019-09-27 11:31:52 +0530

    test

--- functions.php
+++ functions.php
@@ -8,9 +8,9 @@
 
 // This function will enqueue style.css of child theme, feel free to add yours
 function aurum_enqueue_child_theme_scripts() {
-	if(is_front_page()) {
-		wp_enqueue_style( 'aurum-child', get_stylesheet_directory_uri() . '/assets/css/coolstyle.css' );
-	}
+	// if(is_front_page()) {
+	// 	wp_enqueue_style( 'aurum-child', get_stylesheet_directory_uri() . '/assets/css/coolstyle.css' );
+	// }
 }
 
 // Includes
